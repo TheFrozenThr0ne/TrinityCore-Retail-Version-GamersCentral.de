fe4060f63680c44ad546b0280fe517124d417b3f
TheFrozenThr0ne <masterbaron@web.de>
2024-10-11 09:44:53 +0200

    Scripts/FallEvent: Cleanup header and add sql files to SQL custom fallevent

---
+++ sql/custom/world/FallEvent/creature_template_gossip.sql
@@ -0,0 +1,2 @@
+INSERT INTO creature_template_gossip(CreatureID, MenuID, VerifiedBuild) VALUES
+(500010, 600000, 0);
--- src/server/scripts/Custom/FallEvent.cpp
+++ src/server/scripts/Custom/FallEvent.cpp
@@ -1,89 +1,35 @@
 /*******************************************
-* File: FallEventScript.cpp
+* File: FallEvent.cpp
 * Author: TheFrozenThr0ne & CC
 * Description: Manages the custom Fall Event
 *              in the game including NPCs,
 *              objects, and event scripts.
 ********************************************/
 
-#include "ScriptMgr.h"
-#include "ScriptedCreature.h"
-#include "ObjectMgr.h"
-#include "UnitAI.h"
-#include "GameObjectAI.h"
-#include "Map.h"
-#include "Random.h"
-#include "Chat.h"
-#include "Config.h"
-#include "PhasingHandler.h"
-#include <algorithm>
-#include <numeric>
-#include <atomic>
-#include <random>
-#include <MapManager.h>
-#include <unordered_set>
-#include <ctime>
-
-// SQL Statements for Database Configuration
-/*
-# Insert creature template for the "Eventstarter" NPC
-INSERT INTO creature_template (entry, KillCredit1, KillCredit2, name, femaleName, subname, TitleAlt, IconName, RequiredExpansion, VignetteID, faction, npcflag, speed_walk, speed_run, scale, Classification, dmgschool, BaseAttackTime, RangeAttackTime, BaseVariance, RangeVariance, unit_class, unit_flags, unit_flags2, unit_flags3, family, trainer_class, type, VehicleId, AIName, MovementType, ExperienceModifier, RacialLeader, movementId, WidgetSetID, WidgetSetUnitConditionID, RegenHealth, CreatureImmunitiesId, flags_extra, ScriptName, StringId, VerifiedBuild)
-VALUES (555555, 0, 0, 'Eventstarter', NULL, 'Fall Event', NULL, NULL, 0, 0, 35, 1, 1, 1.14286, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 0, 0, 0, 0, 1, 0, 0, 'Eventstarter', NULL, 0);
-
-# Define a creature model to assing it to the NPC
-insert  into creature_template_model(CreatureID,Idx,CreatureDisplayID,DisplayScale,Probability,VerifiedBuild) values
-(500010,0,7337,1,1,0);
-
-# Define a gossip template to assing it to the NPC
-insert  into creature_template_gossip(CreatureID,MenuID,VerifiedBuild) values
-(500010,600000,0);
-
-# Define a gossip menu and assign it to the NPC
-INSERT INTO gossip_menu (MenuID, TextID, VerifiedBuild)
-VALUES (600000, 68, 0);
-
-# Define gameobject templates for the event objects
-INSERT INTO gameobject_template (entry, type, displayId, name, IconName, castBarCaption, unk1, size, Data0, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10, Data11, Data12, Data13, Data14, Data15, Data16, Data17, Data18, Data19, Data20, Data21, Data22, Data23, Data24, Data25, Data26, Data27, Data28, Data29, Data30, Data31, Data32, Data33, Data34, ContentTuningId, AIName, ScriptName, StringId, VerifiedBuild)
-VALUES (555555, 5, 8277, 'FallEvent_object01', '', '', '', 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 'FallObject', NULL, 0);
-
-# Define spawn points for the game objects
-INSERT INTO gameobject (guid, id, map, zoneId, areaId, spawnDifficulties, phaseUseFlags, PhaseId, PhaseGroup, terrainSwapMap, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, ScriptName, StringId, VerifiedBuild)
-VALUES
-(9805103, 555555, 1, 0, 0, '0', 0, 0, 0, -1, -6082.18, -3962.04, 9.8267, 0.446274, -0, -0, -0.22129, -0.975208, 300, 255, 1, '', NULL, 0);
-
-# Define gameobject spawns for the objects
-insert  into gameobject(guid,id,map,zoneId,areaId,spawnDifficulties,phaseUseFlags,PhaseId,PhaseGroup,terrainSwapMap,position_x,position_y,position_z,orientation,rotation0,rotation1,rotation2,rotation3,spawntimesecs,animprogress,state,ScriptName,StringId,VerifiedBuild) values
-(9805103,555555,1,0,0,'0',0,0,0,-1,-6082.18,-3962.04,9.8000,0.446274,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805104,555555,1,0,0,'0',0,0,0,-1,-6077.57,-3971.61,9.8000,0.446795,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805105,555555,1,0,0,'0',0,0,0,-1,-6072.97,-3981.13,9.8000,0.432934,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805106,555555,1,0,0,'0',0,0,0,-1,-6068.48,-3990.79,9.8000,0.446801,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805107,555555,1,0,0,'0',0,0,0,-1,-6071.25,-3957,9.8000,0.427199,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805108,555555,1,0,0,'0',0,0,0,-1,-6066.67,-3966.22,9.8000,0.423437,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805109,555555,1,0,0,'0',0,0,0,-1,-6062.08,-3976.09,9.8000,0.435565,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805110,555555,1,0,0,'0',0,0,0,-1,-6057.46,-3985.86,9.8000,0.423774,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805111,555555,1,0,0,'0',0,0,0,-1,-6060.29,-3951.8,9.8000,3.53371,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805112,555555,1,0,0,'0',0,0,0,-1,-6055.66,-3961.45,9.8000,0.396871,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805113,555555,1,0,0,'0',0,0,0,-1,-6050.95,-3971.2,9.8000,3.5365,-0,-0,-0.2215449,-0.97515,300,255,1,'',NULL,0),
-(9805114,555555,1,0,0,'0',0,0,0,-1,-6046.38,-3980.84,9.8000,0.423919,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805115,555555,1,0,0,'0',0,0,0,-1,-6093.07,-3967.4,9.8000,3.58541,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805116,555555,1,0,0,'0',0,0,0,-1,-6088.43,-3976.92,9.8000,3.58227,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805117,555555,1,0,0,'0',0,0,0,-1,-6083.95,-3986.58,9.8000,0.4385,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0),
-(9805118,555555,1,0,0,'0',0,0,0,-1,-6079.4,-3996.06,9.8000,3.59191,-0,-0,-0.221544,-0.97515,300,255,1,'',NULL,0);
-*/
-
-/* // Condig Statements for worldserver.conf Configuration
+/* // Condig Statements for worldserver.conf
+* 
 FallEvent.Announce = true
+
 */
 
 // Global class for managing the Fall Event
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "ObjectMgr.h"
+#include <MapManager.h>
 #include "UnitAI.h"
 #include "ObjectGuid.h"
 #include "GameObjectAI.h"
 #include "GameObject.h"
 #include "GameObjectInfo.h"
+#include "Chat.h"
+#include "Config.h"
+#include <algorithm>
+#include <numeric>
+#include <atomic>
+#include <random>
+#include <unordered_set>
+#include <ctime>
 
 // Constants
 constexpr int TotalObjects = 16;
