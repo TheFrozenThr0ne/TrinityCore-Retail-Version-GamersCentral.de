6e5465a0d3f3d2d1bfea8bb63db61fbea6b74c03
TheFrozenThr0ne <masterbaron@web.de>
2024-10-11 10:26:12 +0200

    Scripts/npc_allmounts: Cleanup header and add sql files to SQL custom

---
+++ sql/custom/world/NPC All Mounts/creature_template_gossip.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_gossip`(`CreatureID`,`MenuID`,`VerifiedBuild`) VALUES
+(500001,600000,0);
---
+++ sql/custom/world/NPC All Mounts/creature_template_model.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) VALUES
+(500001,0,26571,1,1,56110);
