5be111130ba328230acaf3105fd7ec47627908f0
TheFrozenThr0ne <masterbaron@web.de>
2024-10-11 10:06:44 +0200

    Scripts/reward_shop: Cleanup header and add sql files to SQL custom npc promotion

---
+++ sql/custom/world/NPC Reward Shop/creature_template_gossip.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_gossip`(`CreatureID`,`MenuID`,`VerifiedBuild`) VALUES
+(500002,600000,0);
---
+++ sql/custom/world/NPC Reward Shop/creature_template_model.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) VALUES
+(500002,0,16804,1,1,56110);
