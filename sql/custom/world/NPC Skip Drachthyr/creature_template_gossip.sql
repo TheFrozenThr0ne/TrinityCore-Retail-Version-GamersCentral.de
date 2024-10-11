b69ea1b4ff3030ade1f0ad5c6673987c69e59072
TheFrozenThr0ne <masterbaron@web.de>
2024-10-11 10:21:45 +0200

    Scripts/npc_skipdrachthyr: Cleanup header and add sql files to SQL custom

---
+++ sql/custom/world/NPC Skip Drachthyr/creature_template_gossip.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_gossip`(`CreatureID`,`MenuID`,`VerifiedBuild`) VALUES
+(500004,600000,0);
---
+++ sql/custom/world/NPC Skip Drachthyr/creature_template_model.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) VALUES
+(500004,0,110189,1,1,56110);
