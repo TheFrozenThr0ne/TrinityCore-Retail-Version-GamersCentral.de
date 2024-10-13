9c13d5bdc20f3d2a76fc90c4ef260088c86b39c2
TheFrozenThr0ne <masterbaron@web.de>
2024-10-13 11:53:36 +0200

    Scripts/FallEvent: Optimized

--- sql/custom/world/FallEvent/game_tele.sql
+++ sql/custom/world/FallEvent/game_tele.sql
@@ -1,2 +1,2 @@
-INSERT INTO `game_tele`(`id`,`position_x`,`position_y`,`position_z`,`orientation`,`map`,`name`) VALUES
-(2170,-6089.91,-3949.07,9.24955,5.39037,1,'fallevent');
+INSERT INTO `game_tele` (`id`,`position_x`,`position_y`,`position_z`,`orientation`,`map`,`name`) VALUES
+(2167,-6089.91,-3949.07,9.24955,5.39037,1,'fallevent');
